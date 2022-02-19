e35c794e695b650fdc695643545600d9a8717318
Oktay Mehmed <oktay888@abv.bg>
2022-02-19 21:45:52 +0200

    Implement Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hangman</h1>
+      <p>Choose a letter</p>
+    </>
+  )
+}
+
+export default Header
