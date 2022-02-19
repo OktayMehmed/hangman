d1fe90ac221b5aefac98993c4fdce2a7affbdd4c
Oktay Mehmed <oktay888@abv.bg>
2022-02-20 00:57:09 +0200

    Add loaded word

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Gallow from "./components/Gallow";
-import './App.css'
+import Word from "./components/Word";
+import "./App.css";
+
+const words = [
+  "transcript",
+  "jawbreaker",
+  "fishhook",
+  "flopping",
+  "pneumonia",
+  "syndrome",
+  "grogginess",
+];
+
+const selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const firstLetter = selectedWord.charAt(0);
+const lastLetter = selectedWord.charAt(selectedWord.length - 1);
 
 function App() {
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([])
+
+  useEffect(() => {
+    if (!correctLetters.includes(firstLetter) && !correctLetters.includes(lastLetter)) {
+      setCorrectLetters((correctLetters) => [...correctLetters, firstLetter]);
+      setCorrectLetters((correctLetters) => [...correctLetters, lastLetter]);
+    }
+  }, [correctLetters]);
+
   return (
     <>
       <Header />
       <div className="game-container">
         <Gallow />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   );
--- src/components/Gallow/index.js
+++ src/components/Gallow/index.js
@@ -5,16 +5,16 @@ const Gallow = () => {
   return (
     <>
       <article className="gallow-container">
-        {false && <img src="/images/one.jpg" />}
-        {false && <img src="/images/two.jpg" />}
-        {false && <img src="/images/third.jpg" />}
-        {false && <img src="/images/four.jpg" />}
-        {false && <img src="/images/five.jpg" />}
-        {false && <img src="/images/six.jpg" />}
-        {false && <img src="/images/seven.jpg" />}
-        {false && <img src="/images/eight.jpg" />}
-        {false && <img src="/images/nine.jpg" />}
-        {true && <img src="/images/ten.jpg" />}
+        {false && <img src="/images/one.jpg" alt="hangman"/>}
+        {false && <img src="/images/two.jpg" alt="hangman"/>}
+        {false && <img src="/images/third.jpg" alt="hangman"/>}
+        {false && <img src="/images/four.jpg" alt="hangman"/>}
+        {false && <img src="/images/five.jpg" alt="hangman"/>}
+        {false && <img src="/images/six.jpg" alt="hangman"/>}
+        {false && <img src="/images/seven.jpg" alt="hangman"/>}
+        {false && <img src="/images/eight.jpg" alt="hangman"/>}
+        {false && <img src="/images/nine.jpg" alt="hangman"/>}
+        {true && <img src="/images/ten.jpg" alt="hangman"/>}
       </article>
     </>
   );
